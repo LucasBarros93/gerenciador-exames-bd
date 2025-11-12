175deb9aee62283c156221b193955341d566040b
lucas.barros <lucas.am.barros@usp.br>
2025-11-12 13:51:02 -0300

    initial main

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-from src.screens import Screen
+from src.screens import screen
 
 if __name__ == "__main__":
-    Screen = Screen()
-    Screen.draw_sing_up()
+    while True:
+        Screen = screen.Screen()
+        Screen.draw_welcome()
+
+        option = int(input())
+
+        if(option == 3):
+            break
+
+        if(option == 1):
+            Screen.draw_who()
+            who = int(input())
+
+            Screen.draw_login(who)
+        
+        if(option == 2):
+            Screen.draw_who()
+            who = int(input())
+
+
+
--- src/screens/screen.py
+++ src/screens/screen.py
@@ -5,6 +5,7 @@ class State(Enum):
     WELCOME = 1
     LOGIN = 2
     SING_UP = 3
+    WHO = 4
 
 def clear():
     # Windows usa 'cls', Linux e Mac usam 'clear'
@@ -31,7 +32,21 @@ class Screen:
 
         self.state = State.WELCOME
 
-    def draw_login(self):
+    def draw_who(self):
+        clear()
+
+        print("|----------------|")
+        print("|  Quem e voce?  |")
+        print("|----------------|")
+
+        print()
+        print("[1]: Cidadao")
+        print("[2]: Governo")
+        print("[3]: Hospital")
+
+        self.state = State.WHO
+
+    def draw_login(self, who):
         clear()
         
         print("|----------------|")
@@ -39,14 +54,31 @@ class Screen:
         print("|----------------|")
 
         print()
-        user = input("Usuario: ")
-        password = input("Senha: ")
 
         self.state = State.LOGIN
 
-        return user, password
+        if who == 1:   #cidadao
+            user = input("CPF: ")
+            password = input("Senha: ")
+            return user, password
 
-    def draw_sing_up(self):
+        if who == 2:   #Governo
+            user = input("Tipo de instituicao: ")
+            cnpj = input("CNPJ: ")
+            password = input("Senha: ")
+            return user, cnpj, password
+
+        
+        if who == 3:   #Governo
+            cnpj = input("CNPJ: ")
+            password = input("Senha: ")
+            return cnpj, password
+
+        input("ERROR: opcao invalida")
+        return
+
+
+    def draw_sing_up(self, who):
         clear()
         
         print("|----------------|")
@@ -54,20 +86,29 @@ class Screen:
         print("|----------------|")
 
         print()
-        print("Como deseja se cadastrar?")
-        print("[1]: Cidadao")
-        print("[2]: Governo")
-        print("[3]: Hospital")
 
-        option = input()
-        if option == "1":
-            clear()
+        if who == 1:    #Cidadao
             user = input("Usuario: ")
             password = input("Senha: ")
 
             return user, password
-        if option == 2:
-            return
+
+        if who == 2:   #Governo
+            user = input("Tipo de instituicao: ")
+            cnpj = input("CNPJ: ")
+            password = input("Senha: ")
+
+            return user, cnpj, password
+
+        
+        if who == 3:   #hospital
+            cnpj = input("CNPJ: ")
+            password = input("Senha: ")
+
+            return cnpj, password
+
+        input("ERROR: opcao invalida")
+        return
 
 
         self.state = State.SING_UP
