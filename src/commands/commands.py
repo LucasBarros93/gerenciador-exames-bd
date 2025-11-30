4c47241e8c0cd546daaa447ade2e588adfe29626
lucas.barros <lucas.am.barros@usp.br>
2025-11-30 17:45:05 -0300

    mais um bug fix

--- src/commands/commands.py
+++ src/commands/commands.py
@@ -13,7 +13,7 @@ class GET:
             1: """SELECT idcidadao, cpf, senha FROM idcidadao_cpf
                     WHERE cpf = %s AND senha = %s""",
             2: """SELECT idempresa, cnpj, senha FROM idempresa_cnpj
-                    WHERE cnpj = %s AND senha = %s AND tipo = 'Saúde'""",
+                    WHERE cnpj = %s AND senha = %s""",
         }
 
         self.cursor.execute(sql_query[who], [user, senha])
